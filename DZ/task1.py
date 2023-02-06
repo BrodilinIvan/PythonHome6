f93d18e34dd0ad4881323e999ad81160048f9d56
brodilin ivan <brodilinivan@yandex.ru>
2023-02-06 18:07:05 +0400

    1 Задача решена

--- DZ/task1.py
+++ DZ/task1.py
@@ -5,3 +5,20 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+
+
+def payroll_preparation():
+    try:
+        script_name, total_time, rate_hours, bonus = argv
+        total_time = int(total_time)
+        rate_hours = float(rate_hours)
+        bonus = float(bonus)
+        print(f'Заработная плата сотрудника: '
+              f'{total_time * rate_hours + bonus}')
+    except ValueError:
+        print('Введено некорректное значение!')
+
+
+payroll_preparation()
